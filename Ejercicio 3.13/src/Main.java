130d5a4ad1750ebc091198320a6c5a73a6d948d1
Tomás <tomas.gonzalez@udla.edu.ec>
2025-10-17 20:57:40 -0500

    Update Main.java

--- Ejercicio 3.13/src/Main.java
+++ Ejercicio 3.13/src/Main.java
@@ -1,6 +1,15 @@
 import javax.swing.JOptionPane;
 
+/** 3.13 (La clase Empleado) Cree una clase llamada Empleado, que incluya tres variables de
+ * instancia: un primer nombre (tipo String), un apellido paterno (tipo String) y un salario mensual (double).
+ * Su clase debe tener un constructor que inicialice las tres variables de instancia. Proporcione un método establecer
+ * y un método obtener para cada variable de instancia. Si el salario mensual no es positivo, no establezca su valor.
+ * Escriba una aplicación de prueba llamada PruebaEmpleado, que demuestre las capacidades de la clase Empleado.
+ * Cree dos objetos Empleado y muestre el salario anual de cada objeto. Después, proporcione a cada Empleado un aumento
+ * del 10% y muestre el salario anual de cada Empleado otra vez.*/
+
 public class Main {
+        /** Declaracion de Objetos */
     public static void main(String[] args) {
         Empleado emp1 = new Empleado();
         Empleado emp2 = new Empleado();
